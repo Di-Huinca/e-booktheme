0d56000d0c7f2623562b93ade3418812752ef04f
jeronimo <huincamanjeronimoepet20@gmail.com>
2022-06-16 20:04:36 -0300

    loginForm

--- src/UI/componentes/loginForm.js
+++ src/UI/componentes/loginForm.js
@@ -4,6 +4,8 @@ import { TextInput, Button, View } from "react";
 export default function LoginForm(){
     return(
         <View>
+            <h1>¡Comencemos!</h1>
+            <h2>Use su DNI para ingresar</h2>
             <TextInput placeholder="Email" />
             <TextInput placeholder="Contraseña" />
             <Button title="Enviar" onPress={() => console.log("Enviado")} />
