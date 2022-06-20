c3a66cc58a432a60217b851f9a9dff0a233f91ab
jeronimo <huincamanjeronimoepet20@gmail.com>
2022-06-20 15:25:34 -0300

    NuevaMensaje alert display

---
+++ src/UI/components/NuevaMensaje.jsx
@@ -0,0 +1,12 @@
+import React,{Fragment} from 'react';
+
+export function NuevaMensaje() {
+  return (
+    <Fragment>
+        <h4>Nueva</h4>
+
+        <button>Evaluación</button>
+        <button>Clase</button>
+    </Fragment>
+  );
+}
--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react';
 import { Login } from "./UI/components/Login";
+import { NuevaMensaje } from './UI/components/NuevaMensaje';
 
 export function App() {
   return (
     <Fragment>
         <Login />
+        <NuevaMensaje />
     </Fragment>
   );
 }
