b4473415ed86912408a4c46cf62c4ff7b62f0997
jeronimo <huincamanjeronimoepet20@gmail.com>
2022-06-20 15:36:26 -0300

    ConfirmacionGuardado alert display

---
+++ src/UI/components/ConfirmacionGuardado.jsx
@@ -0,0 +1,13 @@
+import React, {Fragment} from 'react'
+
+export function ConfirmacionGuardado() {
+  return (
+    <Fragment>
+      <h4>¿Desea guardar lo hecho en el dia?</h4>
+
+      <button>Cancelar</button>
+      <button>Confirmar</button>
+
+    </Fragment>
+  )
+}
--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,14 @@
 import React, { Fragment } from 'react';
 import { Login } from "./UI/components/Login";
 import { NuevaMensaje } from './UI/components/NuevaMensaje';
+import { ConfirmacionGuardado } from './UI/components/ConfirmacionGuardado';
 
 export function App() {
   return (
     <Fragment>
         <Login />
         <NuevaMensaje />
+        <ConfirmacionGuardado />
     </Fragment>
   );
 }
