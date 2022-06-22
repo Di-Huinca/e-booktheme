9822d9010253c7eccf832d954a0f3c8c04c0afbb
Lucas <lucasmartindiluca@gmail.com>
2022-06-22 20:27:33 -0300

    LogForm Component Display

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import { Login } from "./UI/components/Login";
 import { NuevaMensaje } from './UI/components/NuevaMensaje';
 import { ConfirmacionGuardado } from './UI/components/ConfirmacionGuardado';
+import { LogForm } from './UI/components/LogForm';
 
 export function App() {
   return (
@@ -9,6 +10,7 @@ export function App() {
         <Login />
         <NuevaMensaje />
         <ConfirmacionGuardado />
+        <LogForm/>
     </Fragment>
   );
 }
