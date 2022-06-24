5c3dfc2a4b5f04fd50d1b4320171461e1c2b0645
jeronimo <huincamanjeronimoepet20@gmail.com>
2022-06-24 15:49:36 -0300

    ClasesLista.jsx Component display

---
+++ src/UI/components/ClasesLista.jsx
@@ -0,0 +1,10 @@
+import React,{Fragment} from 'react';
+import ClasesItem from './ClasesItem';
+
+export function ClasesLista() {
+  return (
+    <Fragment>
+        <ClasesItem/>
+    </Fragment>
+  );
+}
