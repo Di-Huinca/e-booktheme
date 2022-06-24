17c8e69c9343fb5c84c381311315973d9658b740
jeronimo <huincamanjeronimoepet20@gmail.com>
2022-06-24 17:37:03 -0300

    Observaciones.jsx component display

---
+++ src/UI/components/Observaciones.jsx
@@ -0,0 +1,14 @@
+import React,{Fragment} from 'react';
+
+export function Observaciones() {
+  return (
+    <Fragment>
+        <h4>Observaciones</h4>
+        <textarea name="Obsevaciones" id="" cols="30" rows="10" ></textarea><br />
+        <input type="<Navbar>
+            
+        </Navbar>" placeholder='Mensaje'/>
+        <button> ▶ </button>
+    </Fragment>
+  );
+}
--- src/app.jsx
+++ src/app.jsx
@@ -5,7 +5,7 @@ import { ConfirmacionGuardado } from './UI/components/ConfirmacionGuardado';
 import { LogForm } from './UI/components/LogForm';
 import { CambioPass } from './UI/components/CambioPass';
 import { HomeClasesProfesor } from './UI/components/HomeClasesProfesor';
-
+import { Observaciones } from './UI/components/Observaciones';
 
 export function App() {
   return (
