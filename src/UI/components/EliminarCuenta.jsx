8d0532f2f0383937183db9dc9c0b7d62efe23bae
Lucas <lucasmartindiluca@gmail.com>
2022-06-27 15:28:15 -0300

    Eliminar Cuenta component

---
+++ src/UI/components/EliminarCuenta.jsx
@@ -0,0 +1,10 @@
+import React,{Fragment} from 'react'
+
+export function EliminarCuenta() {
+  return (
+    <Fragment>
+        <h4>¿Desea eliminar su cuenta?</h4>
+        <button>Cancelar</button><button>Aceptar</button>
+    </Fragment>
+  )
+}
