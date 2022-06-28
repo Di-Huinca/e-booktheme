c50ba7c389d8274a9d48a46bab5ca0a938a1fbab
Lucas <lucasmartindiluca@gmail.com>
2022-06-28 14:17:59 -0300

    VerificarEmail component display

---
+++ src/UI/components/VerificarEmail.jsx
@@ -0,0 +1,12 @@
+import React,{Fragment} from 'react'
+
+export function VerificarEmail() {
+  return (
+   <Fragment>
+    <h4>Email</h4>
+    <h3>neuquen1234@gmail.com</h3>
+    
+    <h3>Verificar mi Email</h3><button>Verificar</button>
+   </Fragment>
+  )
+}
