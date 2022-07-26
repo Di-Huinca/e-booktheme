fcdd398fa271b82d00a743fd1d734103d06b28b8
jeronimo <huincamanjeronimoepet20@gmail.com>
2022-07-26 18:36:47 -0300

    home clases del profesor duncion comment

--- src/UI/components/HomeClasesProfesor.jsx
+++ src/UI/components/HomeClasesProfesor.jsx
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
 import { ClasesLista } from './ClasesLista';
 
-export function HomeClasesProfesor() {
+export function HomeClasesProfesor() {//home de de las clases del profesor
   return (
     <Fragment>
       <h4>Clases del profesor/a</h4>
