76114e07253da1292dcdf8ce04e0dbb553149442
jeronimo <huincamanjeronimoepet20@gmail.com>
2022-07-26 18:34:40 -0300

    confirmacion de guardado funcion comment

--- src/UI/components/ConfirmacionGuardado.jsx
+++ src/UI/components/ConfirmacionGuardado.jsx
@@ -1,6 +1,6 @@
 import React, {Fragment} from 'react'
 
-export function ConfirmacionGuardado() {
+export function ConfirmacionGuardado() {//mensaje de pop up de confirmacion
   return (
     <Fragment>
       <h4>¿Desea guardar lo hecho en el dia?</h4>
