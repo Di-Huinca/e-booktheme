03de940ccd9fd9f09605aa3a43cea88f24300cb9
Lucas <lucasmartindiluca@gmail.com>
2022-08-24 20:47:44 -0300

    Cambios diseño LogIn

--- src/UI/components/Login.jsx
+++ src/UI/components/Login.jsx
@@ -7,13 +7,13 @@ export function Login() { //log in
       <div className='container'>
         <h1 className='titulo'>¡Comencemos!</h1>
         <h2 className='subtitulo'>Use su DNI para ingresar</h2>
-    <div className='cont-input'></div>
-        <input type='text' placeholder='Username'/><br />
-        <input type='password' name='password' placeholder='Contraseña'/><br />
-
+        <div className='cont-input'>
+        <input type='text' placeholder='Username'/>
+        <input type='password' name='password' placeholder='Contraseña'/>
+       
         <input type='checkbox' name='checkbox' />
-        <label For="checkbox"> Mantener sesion activa</label><br />
-
+        <label For="checkbox"> Mantener sesion activa</label>
+        </div>
         <button className='boton'>INGRESAR</button>
 
         <div className='info'>No tenes cuenta? <a href="#">Regístrate aquí.</a></div>
