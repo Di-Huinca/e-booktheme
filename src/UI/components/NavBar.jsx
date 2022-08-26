a2e4ddeff3628b5bc623710225273b9488671aca
jeronimo <huincamanjeronimoepet20@gmail.com>
2022-08-26 19:11:00 -0300

    navbar component

--- src/UI/components/NavBar.jsx
+++ src/UI/components/NavBar.jsx
@@ -1,11 +1,26 @@
-import React from "react";
-import { Fragment } from "react";
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
-
-export function NavBar(){
-    return (
-        <Fragment>
-            
-        </Fragment>
-    );
+export default function NavBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
