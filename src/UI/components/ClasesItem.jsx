7b9164729d91bc4b22c3e62ea566015c6593faec
jeronimo <huincamanjeronimoepet20@gmail.com>
2022-08-26 20:49:49 -0300

    clasesItem component

--- src/UI/components/ClasesItem.jsx
+++ src/UI/components/ClasesItem.jsx
@@ -1,20 +1,11 @@
 import React, {Fragment} from 'react'
-import { ClasesAnteriores } from './ClasesAnteriores';
 
 export function ClasesItem() {//item de calses del dia
   return (
     <Fragment>
-        <h1>Nombre materia</h1>
-        <p>Horarios y dias de la semana</p>
-
-        <h3>Hoy</h3>
-        <p>Actualizacion del dia con estados de si hay o no contenido</p>
-
-        <h3>Anteriores</h3>
-        <ClasesAnteriores/>
-
-        <button>+</button>
-
+      <div className=''>
+        <h6> Nombre materia </h6>
+      </div>
     </Fragment>
   );
 }
