b73485cb486b355d352df9178c024ed30768f970
jeronimo <huincamanjeronimoepet20@gmail.com>
2022-08-25 21:26:06 -0300

    limpieza

--- src/app.jsx
+++ src/app.jsx
@@ -1,30 +1,11 @@
 import React, { Fragment } from 'react';
-import { Login } from "./UI/components/Login";
-import { NuevaMensaje } from './UI/components/NuevaMensaje';
-import { ConfirmacionGuardado } from './UI/components/ConfirmacionGuardado';
-import { LogForm } from './UI/components/LogForm';
-import { CambioPass } from './UI/components/CambioPass';
-import { HomeClasesProfesor } from './UI/components/HomeClasesProfesor';
-import { CerrarSesion } from './UI/components/CerrarSesion';
-
-import { EliminarCuenta } from './UI/components/EliminarCuenta';
-
-import { Materia } from './UI/components/Materia';
-
-import { VerificarEmail } from './UI/components/VerificarEmail';
-
-
-
-
-  import HomeScreen from './UI/components/HomeScreen'
-  import MateriaScreen from './UI/components/Materia'
-
-
+import { NavBar } from './UI/components/NavBar';
 
   export function App() {
     return (
       <Fragment>
-         hola como estas?
+         hola como estas? 
+    
        </Fragment>
      );
 }
