ddc5e3bd8a10fcb0eb75c47fcb268436ca39b958
jeronimo <huincamanjeronimoepet20@gmail.com>
2022-08-26 21:04:39 -0300

    clasesItem pruebas

--- src/UI/components/ClasesLista.jsx
+++ src/UI/components/ClasesLista.jsx
@@ -1,10 +1,14 @@
+import { Typography } from '@mui/material';
 import React,{Fragment} from 'react';
 import { ClasesItem } from './ClasesItem';
 
 export function ClasesLista() {//lista cada item de la clase
   return (
     <Fragment>
-        <ClasesItem/>
+      <div>
+      <Typography variant='h5' component='h5'>Clases</Typography>
+      <ClasesItem></ClasesItem>
+      </div>
     </Fragment>
   );
 }
