fc4595a92470e6cbee78c6b71443655c494c9bd3
Lucas <lucasmartindiluca@gmail.com>
2022-08-29 10:11:40 -0300

    Cambios app

--- src/app.jsx
+++ src/app.jsx
@@ -1,30 +1,12 @@
 import React, { Fragment } from 'react';
-import { Login } from "./UI/components/Login";
-import { NuevaMensaje } from './UI/components/NuevaMensaje';
-import { ConfirmacionGuardado } from './UI/components/ConfirmacionGuardado';
-import { LogForm } from './UI/components/LogForm';
 import { CambioPass } from './UI/components/CambioPass';
-import { HomeClasesProfesor } from './UI/components/HomeClasesProfesor';
-import { CerrarSesion } from './UI/components/CerrarSesion';
-
-import { EliminarCuenta } from './UI/components/EliminarCuenta';
-
-import { Materia } from './UI/components/Materia';
-
-import { VerificarEmail } from './UI/components/VerificarEmail';
-
-
-
-
-  import HomeScreen from './UI/components/HomeScreen'
-  import MateriaScreen from './UI/components/Materia'
-
-
+import { Login } from './UI/components/Login';
 
   export function App() {
     return (
       <Fragment>
-         <CambioPass></CambioPass>
+            <CambioPass></CambioPass>
+            <Login></Login>
        </Fragment>
      );
 }
