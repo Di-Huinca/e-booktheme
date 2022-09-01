7cbc6c620e234024bce55638a8eefa2d51087945
Lucas <lucasmartindiluca@gmail.com>
2022-08-31 21:21:31 -0300

    Component ConfirmSave - Listo

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,9 @@
 import React, { Fragment } from 'react';
-import { CambioPass } from './UI/components/CambioPass';
-import { Login } from './UI/components/Login';
 import { ConfirmacionGuardado } from './UI/components/ConfirmacionGuardado';
  
 export function App() {
     return (
       <Fragment>
-            <CambioPass></CambioPass>
-            <Login></Login>
             <ConfirmacionGuardado></ConfirmacionGuardado>
        </Fragment>
      );
