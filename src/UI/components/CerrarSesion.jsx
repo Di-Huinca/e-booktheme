5ae5410781273a04b9e6941238d595224a095b4e
Lucas <lucasmartindiluca@gmail.com>
2022-08-31 22:07:26 -0300

    EliminarCuenta - Componente finalizado

--- src/UI/components/CerrarSesion.jsx
+++ src/UI/components/CerrarSesion.jsx
@@ -1,10 +1,23 @@
 import React,{Fragment} from 'react'
+import  './CerrarSesion.css'
+
 
 export function CerrarSesion() {//mensaje de pop up preguntado si desea cerrar sesion
   return (
     <Fragment>
-        <h3>¿Desea cerrar sesión?</h3>
-        <button>Cancelar</button><button>Aceptar</button>
+      <div className='contenedor'>
+        <h3 className='texto'>¿Desea cerrar sesión?</h3>
+          <div>
+
+            <div className='button'>
+                <button className='btn'>Cancelar</button>
+                <button className='btn'>Aceptar</button>
+            </div>
+        
+        </div>
+    
+    </div>  
+    
     </Fragment>
   );
 }
--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,16 @@
 import React, { Fragment } from 'react';
+import { CerrarSesion } from './UI/components/CerrarSesion';
 import { ConfirmacionGuardado } from './UI/components/ConfirmacionGuardado';
- 
+import { EliminarCuenta } from './UI/components/EliminarCuenta';
+
+
 export function App() {
+
     return (
       <Fragment>
+            <CerrarSesion></CerrarSesion>
             <ConfirmacionGuardado></ConfirmacionGuardado>
+            <EliminarCuenta></EliminarCuenta>
        </Fragment>
      );
 }
