8e55488d85809aa0363d400d0799b66f11adfdfe
jeronimo <huincamanjeronimoepet20@gmail.com>
2022-09-01 13:13:33 -0300

    Cambio de nombre

--- src/app.jsx
+++ src/app.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter,Routes,Route } from "react-router-dom";
 import { HomeClasesProfesor } from './UI/routes/HomeClasesProfesor';
 import HomeScreen from './UI/routes/HomeScreen.jsx';
 import {Login} from './UI/routes/Login.jsx' 
+import {LogForm} from './UI/routes/LogForm.jsx' 
+import { CerrarSesion } from './UI/components/CerrarSesion';
 
   export function App() {
     return (
@@ -11,6 +13,8 @@ import {Login} from './UI/routes/Login.jsx'
           <Route path='/' element={<HomeScreen/>} />
           <Route path='home/profesor' element={<HomeClasesProfesor/>} />
           <Route path='login' element={<Login/>}></Route>
+          <Route path='logform' element={<LogForm/>} />
+          <Route path='cerrar-sesion' element={<CerrarSesion/>} />
       </Routes>
 </BrowserRouter>
      );
