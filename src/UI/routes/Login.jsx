9f63717b932caa8b7cf79a7a8107dc25d50266ec
jeronimo <huincamanjeronimoepet20@gmail.com>
2022-09-02 13:15:59 -0300

    login routes

--- src/UI/routes/Login.jsx
+++ src/UI/routes/Login.jsx
@@ -10,13 +10,9 @@ export function Login() { //log in
         <h2 className='subtitulo'>Use su DNI para ingresar</h2>
         <div className='cont-input'>
         <input className='text' type='text' placeholder='Username'/>
-        <input className='password' type='password' name='password' placeholder='Password'/>
-          <div className='cont-sesion'>       
-            <input className='checkbox' type='checkbox' name='checkbox' />
-            <label For="checkbox"> Mantener sesion activa</label>
-          </div>
+        <input className='password' type='password' placeholder='Password'/>
         </div>
-        <button className='boton'>INGRESAR</button>
+        <Link to='/'> <button className='boton'>INGRESAR</button></Link>
 
         <div className='info'>No tenes cuenta? <Link to='/logform'>Regístrate aquí</Link></div>
         </div>
