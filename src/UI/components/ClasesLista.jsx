d140448e9d71f34813489530014c720ebfbef74d
jeronimo <huincamanjeronimoepet20@gmail.com>
2022-09-08 15:03:50 -0300

    ClasesLista component: *utilizacion de props para datos como materia, curso y horario.

--- src/UI/components/ClasesLista.jsx
+++ src/UI/components/ClasesLista.jsx
@@ -1,14 +1,22 @@
 import { Typography } from '@mui/material';
-import React,{Fragment} from 'react';
+import React from 'react';
 import { ClasesItem } from './ClasesItem';
 
 export function ClasesLista() {//lista cada item de la clase
   return (
-    <Fragment>
-      <div>
-      <Typography variant='h5' component='h5'>Clases</Typography>
-      <ClasesItem></ClasesItem>
-      </div>
-    </Fragment>
+    <div className='lista-container'>
+      <Typography className='titulo-clases' variant='h5'>Clases</Typography>
+      <ClasesItem 
+        materia='Inglés'
+        curso='6°1°'
+        horario='15:00hs a 18:00hs'
+      />
+      <ClasesItem 
+        materia='Seguridad informatica'
+        curso='5°1°'
+        horario='13:00hs a 18:00hs'
+      />
+      
+    </div>
   );
 }
