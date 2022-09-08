e276524e42c43fa1b67da353e66208a4ff26eed6
jeronimo <huincamanjeronimoepet20@gmail.com>
2022-09-08 12:38:37 -0300

    navbar component

--- src/UI/components/NavBar.jsx
+++ src/UI/components/NavBar.jsx
@@ -1,26 +1,24 @@
-import * as React from 'react';
+import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import ArrowBack from '@mui/icons-material/ArrowBack';
 
-export default function NavBar() {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="arrow"
-            sx={{ mr: 2 }}
-          >
-            <ArrowBack/>
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-    </Box>
+export function NavBar() {
+  return(
+    <Box className='container' sx={{ flexGrow: 1 }}>
+    <AppBar position="static">
+      <Toolbar>
+      <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+        >
+          <ArrowBack/>
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  </Box>
   );
-}
+  }
