320708b66955281306aef204b9f2ea4d8012085d
jeronimo <huincamanjeronimoepet20@gmail.com>
2022-09-08 13:37:48 -0300

    nuevos redireccionamientos

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BrowserRouter,Routes,Route } from "react-router-dom";
-
+import Login from './UI/routes/Login';
+import HomeScreen from './UI/routes/HomeScreen';
 
   export function App() {
     return (
+
       <BrowserRouter>
         <Routes>
-            <Route></Route>
+          <Route path='/' element={<Login/>}/>
+          <Route path='/home' element={<HomeScreen/>}/>
+          
         </Routes>
       </BrowserRouter>
      );
