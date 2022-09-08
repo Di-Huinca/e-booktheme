00ed998234bdc67c48016c8b515e7a3cde924ae6
jeronimo <huincamanjeronimoepet20@gmail.com>
2022-09-08 19:23:19 -0300

    Clases item y Clases Lista *Creacion de un array para que se creen objetos de componenteste automaticamente.

--- src/UI/components/ClasesItem.jsx
+++ src/UI/components/ClasesItem.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './ClasesItem.css'
 
-export function ClasesItem(props) {//item de calses del dia
+export function ClasesItem({clases, props}) {//item de calses del dia
+  console.log(clases.materia);
   return (
   <div className='clases-item-container'> 
-      <p>{props.materia} {props.curso} {props.horario} </p>
+      <p>{clases.materia} {clases.curso} {clases.horario} </p>
   </div>
   );
 }
--- src/UI/components/ClasesLista.jsx
+++ src/UI/components/ClasesLista.jsx
@@ -2,21 +2,24 @@ import { Typography } from '@mui/material';
 import React from 'react';
 import { ClasesItem } from './ClasesItem';
 
+//creo un array de objetos en el cual al llamarlo desde el componente creara cada item por cada objeto
+const clases = [ 
+  {id: 1,curso: "5°1°",horario:"17:00 a 18:00", materia: "english"},
+  {id: 2,curso: "6°1°",horario:"15:00 a 18:00", materia: "english"},
+  {id: 3,curso: "6°1°",horario:"15:00 a 18:00", materia: "so 2"},
+];
+
+
 export function ClasesLista() {//lista cada item de la clase
   return (
     <div className='lista-container'>
       <Typography className='titulo-clases' variant='h5'>Clases</Typography>
-      <ClasesItem 
-        materia='Inglés'
-        curso='6°1°'
-        horario='15:00hs a 18:00hs'
-      />
-      <ClasesItem 
-        materia='Seguridad informatica'
-        curso='5°1°'
-        horario='13:00hs a 18:00hs'
-      />
-      
+
+        {clases.map((clase) =>            //cremos un array.map del arreglo clases. por cada elemento del arreglo, crea un apodo para relacionado, e imprime el componente dentro del array.map
+          <ClasesItem key={clase.id}      //el componente tiene un prop key que lo busca el atributo id de la constante 'clases', para que sea unico.
+            clases={clase}         //creamos el prop clases y le enviamos el elemento con sus atributos y valores.
+          />
+        )}
     </div>
   );
 }
