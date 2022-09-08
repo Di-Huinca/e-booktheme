cadb968cdd8b8713b54dada2e2321c42c603c6eb
jeronimo <huincamanjeronimoepet20@gmail.com>
2022-09-08 15:04:44 -0300

    clases item: *utilizacion de props pra materia, curso y horario. *estilizacion basica.

--- src/UI/components/ClasesItem.jsx
+++ src/UI/components/ClasesItem.jsx
@@ -1,12 +1,10 @@
-import React, {Fragment} from 'react'
+import React from 'react'
 import './ClasesItem.css'
 
-export function ClasesItem() {//item de calses del dia
+export function ClasesItem(props) {//item de calses del dia
   return (
-    <Fragment>
-      <div className='container'>
-        <h5> Nombre materia </h5>
-      </div>
-    </Fragment>
+  <div className='clases-item-container'> 
+      <p>{props.materia} {props.curso} {props.horario} </p>
+  </div>
   );
 }
