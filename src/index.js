c14cdaef673e7b295bf16a03aac205bbd092a272
jeronimo <huincamanjeronimoepet20@gmail.com>
2022-10-17 18:27:25 -0300

    react-bootstrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { App } from "./app.jsx";
+import { App } from "./App.jsx";
 import './index.css'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const root = ReactDOM.createRoot(
