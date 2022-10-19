9a9a5efb07838fe24c1cfe01cdd91ff63f0399c9
jeronimo <huincamanjeronimoepet20@gmail.com>
2022-10-19 16:36:56 -0300

    App enrutados y firebase

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,40 @@
 
-import React from 'react';
+import React,{ useState } from 'react';
 import { BrowserRouter,Routes,Route } from "react-router-dom";
+
 import {Login} from "./UI/routes/Login"
+import {LogForm} from "./UI/routes/LogForm"
+import Home from './UI/routes/Home';
+
+import firebaseApp from './firebase/firebase';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+
+const auth = getAuth(firebaseApp);
 
   export function App() {
-    return (
+    const [usuarioGlobal, setUsuarioGlobal] = useState(null);
+
+    onAuthStateChanged(auth, (usuariofirebase) => {
+      if (usuariofirebase){
+        //si hay sesion iniciada...
+        setUsuarioGlobal(usuariofirebase);
+      }else{
+        //si no hay sesion iniciada...
+        setUsuarioGlobal(null);
+      }
+    })
+
+    return (<>
       <BrowserRouter>
         <Routes>
-            <Route path='/' element={<Login/>}>
-            </Route>
+            <Route path='/' element={<Login/>}/>
+            <Route path='logform' element={<LogForm/>}/>
+            <Route path='home' element={<Home/>}/>
+            
         </Routes>
       </BrowserRouter>
+
+      {usuarioGlobal ? <Home correoUsuario={usuarioGlobal.email}/> : <Login/>}
+      </>
      );
 }
