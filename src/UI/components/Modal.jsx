6d59b2e54c462da34919fac45838610c19d4a518
Lucas <lucasmartindiluca@gmail.com>
2022-11-11 20:48:53 -0300

    Modal * Form New Class * Import New Class in Home

---
+++ src/UI/components/Modal.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+
+function ModalExample() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Crear
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title
+            style={{ width: "150px" }}
+          >Nueva Clase
+          </Modal.Title>
+
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Tema del dia</Form.Label>
+              <Form.Control
+                type="input"
+                placeholder="Ej: Polinomios"
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Observaciones</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Cerrar
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Guardar cambios
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default ModalExample
+// render(<Example />);
--- src/UI/routes/Home.jsx
+++ src/UI/routes/Home.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 
 import '../routes/Home.css'
 import Sidebar from '../components/Sidebar'
@@ -9,56 +9,61 @@ import MainCont from '../components/MainCont';
 // import  ListarMaterias  from '../components/ListarMaterias';
 // import AgregarEvaluacion from '../components/AgregarEvaluacion';
 
-import { firebaseApp } from '../../firebase/firebase'
+import firebaseApp from '../../firebase/firebase'
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
-import { getAuth} from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
+import ModalExample from '../components/Modal';
 
 const firestore = getFirestore(firebaseApp);
 const auth = getAuth(firebaseApp)
 
 
-const Home = ({correoUsuario}) => {
- 
-  const [arrayEvaluaciones,setArrayEvaluaciones] = useState(null); 
-  const [arrayTareas,setArrayTareas] = useState(null); 
+const Home = ({ correoUsuario }) => {
+
+  const [arrayEvaluaciones, setArrayEvaluaciones] = useState(null);
+  const [arrayTareas, setArrayTareas] = useState(null);
 
   const fakeData = [
-    { id:1, 
-      nombre:"jeronimo", 
-      descripcion:"prueba falsa para probar"},
-    { id:2, 
-      nombre:"hola hola", 
-      descripcion:"prueba falsa 2 "}
+    {
+      id: 1,
+      nombre: "jeronimo",
+      descripcion: "prueba falsa para probar"
+    },
+    {
+      id: 2,
+      nombre: "hola hola",
+      descripcion: "prueba falsa 2 "
+    }
   ]
 
-  async function buscarDocumentoOrCrearDocumento(idDocumento){
+  async function buscarDocumentoOrCrearDocumento(idDocumento) {
     //crear referencia al documento
     const docuRef = doc(firestore, `usuarios/${idDocumento}`);
     //buscar documento
     const consulta = await getDoc(docuRef);
     //revisa si existe 
-    if (consulta.exists()){
+    if (consulta.exists()) {
       //si existe
       const infoDocu = consulta.data();
       return infoDocu.evaluaciones;
-    }else{
+    } else {
       //si no existe
-      await setDoc(docuRef, {evaluaciones: [...fakeData] });
+      await setDoc(docuRef, { evaluaciones: [...fakeData] });
       const consulta = await getDoc(docuRef);
       const infoDocu = consulta.data();
       return infoDocu.evaluaciones;
-      
+
     }
   }
 
-  useEffect (() => {
-    async function obtenerEvaluaciones(){
+  useEffect(() => {
+    async function obtenerEvaluaciones() {
       const evalObtenidas = await buscarDocumentoOrCrearDocumento(correoUsuario);
       setArrayEvaluaciones(evalObtenidas);
     }
-      obtenerEvaluaciones();
+    obtenerEvaluaciones();
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   // useEffect(() => {
@@ -69,30 +74,36 @@ const Home = ({correoUsuario}) => {
   //     obtenerTareas();
   // }, [])
 
-  return(
-
-      //     <AgregarEvaluacion
-      //       arrayEvaluaciones={arrayEvaluaciones}
-      //       setArrayEvaluaciones={setArrayEvaluaciones}
-      //       correoUsuario={correoUsuario}
-      //     />
-
-      //     {arrayEvaluaciones ? ( <ListarEvaluaciones arrayEvaluaciones={arrayEvaluaciones}
-      //     setArrayEvaluaciones={setArrayEvaluaciones}
-      //     correoUsuario={correoUsuario} />) : null}
-      //     {arrayTareas ? ( <ListarMaterias arrayTareas={arrayTareas}
-      //     setArrayTareas={setArrayTareas}
-      //     correoUsuario={correoUsuario} />) : null}
 
 
-      <>
-          <div className="home">
-              <div className="sidebar-container"><Sidebar auth={auth}/></div>
-              <div className="header-container"><Head/></div>
-              <div className="main-panel"><MainCont/></div>
-          </div>
-      </>
-    );
+  return (
+
+    //     <AgregarEvaluacion
+    //       arrayEvaluaciones={arrayEvaluaciones}
+    //       setArrayEvaluaciones={setArrayEvaluaciones}
+    //       correoUsuario={correoUsuario}
+    //     />
+
+    //     {arrayEvaluaciones ? ( <ListarEvaluaciones arrayEvaluaciones={arrayEvaluaciones}
+    //     setArrayEvaluaciones={setArrayEvaluaciones}
+    //     correoUsuario={correoUsuario} />) : null}
+    //     {arrayTareas ? ( <ListarMaterias arrayTareas={arrayTareas}
+    //     setArrayTareas={setArrayTareas}
+    //     correoUsuario={correoUsuario} />) : null}
+
+
+    <>
+      <div className="home">
+        <div className="sidebar-container"><Sidebar auth={auth} /></div>
+        <div className="header-container"><Head /></div>
+
+        <div className="main-panel">
+          <ModalExample />
+          <MainCont />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
